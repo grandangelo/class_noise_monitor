f5c68c77ad43f8752eae08fc149949f324583309
grandangelo <grandangelo@zoho.com>
2023-02-04 18:55:56 +0100

    Add view model

---
+++ VS/Code/ClassNoiseMonitor/ClassNoiseMonitor/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassNoiseMonitor
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region Private Members
+        #endregion
+
+        #region Public Members
+        public int CurrentVolume { get; set; }
+        #endregion
+
+        #region Constructor
+        public MainWindowViewModel()
+        {
+            CurrentVolume = 10;
+        }
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #region Private Members
+        #endregion
+
+        #region INotify Support
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
