7a54fafec1a0603a40411729b3fce94369f05b65
grandangelo <grandangelo@zoho.com>
2023-02-05 10:42:56 +0100

    Add messages

--- VS/Code/ClassNoiseMonitor/ClassNoiseMonitor/MainWindowViewModel.cs
+++ VS/Code/ClassNoiseMonitor/ClassNoiseMonitor/MainWindowViewModel.cs
@@ -13,14 +13,21 @@ namespace ClassNoiseMonitor
     internal class MainWindowViewModel : INotifyPropertyChanged, IDisposable
     {
         #region Private Members
+        private const int _mediumNoiseThreshold = 10;
+        private const int _highNoiseThreshold = 40;
+        private static readonly string _lowNoiseMessage = "Ottimo!";
+        private static readonly string _mediumNoiseMessage = "Ragazzi attenzione...";
+        private static readonly string _highNoiseMessage = "SILENZIO!";
         private readonly Model _model;
         private readonly CancellationTokenSource _cts;
         private readonly Task _monitoringTask;
         private int _currentVolume;
+        private string _labelContent;
         #endregion
 
         #region Public Members
         public int CurrentVolume { get => _currentVolume; set { if (value == _currentVolume) return; _currentVolume = value; OnPropertyChanged(); } }
+        public string LabelContent { get => _labelContent; set { if (value == _labelContent) return; _labelContent = value; OnPropertyChanged(); } }
         #endregion
 
         #region Constructor
@@ -30,7 +37,9 @@ namespace ClassNoiseMonitor
             _model = new Model();
             _model.UpdatedVolumeEvent += OnUpdatedVolumeEvent;
             _monitoringTask = Task.Run(() => _model.StartMonitoring(_cts.Token));
+            _labelContent = string.Empty;
             CurrentVolume = 0;
+            LabelContent = string.Empty;
         }
         #endregion
 
@@ -44,6 +53,17 @@ namespace ClassNoiseMonitor
         private void OnUpdatedVolumeEvent(object? sender, VolumeUpdateEvent e)
         {
             CurrentVolume = e.UpdateVolume;
+            LabelContent = GetLabelContent();
+        }
+
+        private string GetLabelContent()
+        {
+            return CurrentVolume switch
+            {
+                > _highNoiseThreshold => _highNoiseMessage,
+                > _mediumNoiseThreshold => _mediumNoiseMessage,
+                _ => _lowNoiseMessage,
+            };
         }
         #endregion
 
